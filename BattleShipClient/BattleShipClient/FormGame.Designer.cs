6f8a44ba955d4e3d15ae6bf4c5e97d711c5c55cc
canadiandude <francoisrioux67@hotmail.com>
2015-10-14 10:55:12 -0400

    Début du Client

---
+++ BattleShipClient/BattleShipClient/FormGame.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BattleShipClient
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV_AllyFleet = new System.Windows.Forms.DataGridView();
+            this.GRB_AllyFleet = new System.Windows.Forms.GroupBox();
+            this.GRB_EnemyFleet = new System.Windows.Forms.GroupBox();
+            this.DGV_EnemyFleet = new System.Windows.Forms.DataGridView();
+            this.PNL_InfoBar = new System.Windows.Forms.Panel();
+            this.LBL_Status = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_AllyFleet)).BeginInit();
+            this.GRB_AllyFleet.SuspendLayout();
+            this.GRB_EnemyFleet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_EnemyFleet)).BeginInit();
+            this.PNL_InfoBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DGV_AllyFleet
+            // 
+            this.DGV_AllyFleet.AllowUserToAddRows = false;
+            this.DGV_AllyFleet.AllowUserToDeleteRows = false;
+            this.DGV_AllyFleet.AllowUserToResizeColumns = false;
+            this.DGV_AllyFleet.AllowUserToResizeRows = false;
+            this.DGV_AllyFleet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DGV_AllyFleet.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.DGV_AllyFleet.Location = new System.Drawing.Point(6, 19);
+            this.DGV_AllyFleet.MultiSelect = false;
+            this.DGV_AllyFleet.Name = "DGV_AllyFleet";
+            this.DGV_AllyFleet.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DGV_AllyFleet.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.DGV_AllyFleet.Size = new System.Drawing.Size(352, 322);
+            this.DGV_AllyFleet.TabIndex = 0;
+            this.DGV_AllyFleet.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_AllyFleet_CellClick);
+            // 
+            // GRB_AllyFleet
+            // 
+            this.GRB_AllyFleet.Controls.Add(this.DGV_AllyFleet);
+            this.GRB_AllyFleet.Location = new System.Drawing.Point(13, 13);
+            this.GRB_AllyFleet.Name = "GRB_AllyFleet";
+            this.GRB_AllyFleet.Size = new System.Drawing.Size(367, 350);
+            this.GRB_AllyFleet.TabIndex = 1;
+            this.GRB_AllyFleet.TabStop = false;
+            this.GRB_AllyFleet.Text = "Votre flotte";
+            // 
+            // GRB_EnemyFleet
+            // 
+            this.GRB_EnemyFleet.Controls.Add(this.DGV_EnemyFleet);
+            this.GRB_EnemyFleet.Location = new System.Drawing.Point(386, 13);
+            this.GRB_EnemyFleet.Name = "GRB_EnemyFleet";
+            this.GRB_EnemyFleet.Size = new System.Drawing.Size(367, 350);
+            this.GRB_EnemyFleet.TabIndex = 2;
+            this.GRB_EnemyFleet.TabStop = false;
+            this.GRB_EnemyFleet.Text = "Flotte ennemi";
+            // 
+            // DGV_EnemyFleet
+            // 
+            this.DGV_EnemyFleet.AllowUserToAddRows = false;
+            this.DGV_EnemyFleet.AllowUserToDeleteRows = false;
+            this.DGV_EnemyFleet.AllowUserToResizeColumns = false;
+            this.DGV_EnemyFleet.AllowUserToResizeRows = false;
+            this.DGV_EnemyFleet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DGV_EnemyFleet.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.DGV_EnemyFleet.Location = new System.Drawing.Point(6, 19);
+            this.DGV_EnemyFleet.MultiSelect = false;
+            this.DGV_EnemyFleet.Name = "DGV_EnemyFleet";
+            this.DGV_EnemyFleet.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DGV_EnemyFleet.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.DGV_EnemyFleet.Size = new System.Drawing.Size(352, 322);
+            this.DGV_EnemyFleet.TabIndex = 0;
+            // 
+            // PNL_InfoBar
+            // 
+            this.PNL_InfoBar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PNL_InfoBar.Controls.Add(this.LBL_Status);
+            this.PNL_InfoBar.Location = new System.Drawing.Point(13, 370);
+            this.PNL_InfoBar.Name = "PNL_InfoBar";
+            this.PNL_InfoBar.Size = new System.Drawing.Size(740, 47);
+            this.PNL_InfoBar.TabIndex = 3;
+            // 
+            // LBL_Status
+            // 
+            this.LBL_Status.AutoSize = true;
+            this.LBL_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_Status.Location = new System.Drawing.Point(3, 10);
+            this.LBL_Status.Name = "LBL_Status";
+            this.LBL_Status.Size = new System.Drawing.Size(218, 25);
+            this.LBL_Status.TabIndex = 0;
+            this.LBL_Status.Text = "En attente du serveur";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 429);
+            this.Controls.Add(this.PNL_InfoBar);
+            this.Controls.Add(this.GRB_EnemyFleet);
+            this.Controls.Add(this.GRB_AllyFleet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_AllyFleet)).EndInit();
+            this.GRB_AllyFleet.ResumeLayout(false);
+            this.GRB_EnemyFleet.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_EnemyFleet)).EndInit();
+            this.PNL_InfoBar.ResumeLayout(false);
+            this.PNL_InfoBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV_AllyFleet;
+        private System.Windows.Forms.GroupBox GRB_AllyFleet;
+        private System.Windows.Forms.GroupBox GRB_EnemyFleet;
+        private System.Windows.Forms.DataGridView DGV_EnemyFleet;
+        private System.Windows.Forms.Panel PNL_InfoBar;
+        private System.Windows.Forms.Label LBL_Status;
+    }
+}
+
---
+++ BattleShipClient/BattleShipClient/FormGame.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleShipClient
+{
+    public partial class FormGame : Form
+    {
+        private bool isPlacingShip = false;
+        private ShipManager shipManager = new ShipManager();
+
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            buildDGV(DGV_AllyFleet);
+            buildDGV(DGV_EnemyFleet);
+            UpdateStatusLabel_ShipPlacement();
+        }
+
+        private void buildDGV(DataGridView DGV)
+        {
+            DGV.ColumnHeadersHeight = 20;
+            DGV.RowHeadersWidth = 50;
+
+            for (int i = 0; i < 10; i++)
+            {
+                DGV.Columns.Add("Col" + (i + 1).ToString(), (i + 1).ToString());
+                DGV.Columns[i].Width = 30;
+            }
+
+            char rowHeader = 'A';
+
+            for (int i = 0; i < 10; i++)
+            {
+                DGV.Rows.Add();
+                DGV.Rows[i].HeaderCell.Value = rowHeader.ToString();
+                DGV.Rows[i].Height = 30;
+                rowHeader++;
+            }
+        }
+
+        private void DGV_AllyFleet_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            int col = e.ColumnIndex;
+
+            if (row != -1 && col != -1)
+            {
+                if (isPlacingShip)
+                {
+                    if (DGV_AllyFleet[col, row].Style.BackColor == Color.Green)
+                    {
+                        placeShipOnGrid(col, row);
+                        resetShipPlacement();
+                        isPlacingShip = false;
+                        shipManager.CurrentShipIndex++;
+
+                        if (shipManager.CurrentShipIndex == ShipManager.ShipTypes.SIZEOF_SHIPNAMES)
+                        {
+                            LBL_Status.Text = "En attente de l'autre joueur";
+                        }
+                        else
+                        {
+                            UpdateStatusLabel_ShipPlacement();
+                        }
+                        
+                    }
+                    else if (DGV_AllyFleet[col, row].Style.BackColor == Color.Blue)
+                    {
+                        resetShipPlacement();
+                        isPlacingShip = false;
+                    }
+                }
+                else if (DGV_AllyFleet[col, row].Style.BackColor != Color.Black)
+                {
+                    shipManager.CurrentShipPosition.X = col;
+                    shipManager.CurrentShipPosition.Y = row;
+                    DGV_AllyFleet[col, row].Style.BackColor = Color.Blue;
+                    placeShipMarkers(col, row, shipManager.ShipeSizes[(int)shipManager.CurrentShipIndex] - 1);
+                    isPlacingShip = true;
+                }
+            }
+
+            DGV_AllyFleet.ClearSelection();
+        }
+
+        private void placeShipMarkers(int col, int row, int shipSize)
+        {
+            for (int c = -1; c <= 1; c++)
+            {
+                for (int r = -1; r <= 1; r++)
+                {
+                    if ((c == 0) != (r == 0))
+                    {
+                        if (row + (r * shipSize) >= 0 && row + (r * shipSize) < DGV_AllyFleet.Rows.Count &&
+                            col + (c * shipSize) >= 0 && col + (c * shipSize) < DGV_AllyFleet.Columns.Count &&
+                            !shipIsInTheWay(col + (c * shipSize), row + (r * shipSize)))
+                        {
+                            DGV_AllyFleet[col + (c * shipSize), row + (r * shipSize)].Style.BackColor = Color.Green;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void placeShipOnGrid(int col, int row)
+        {
+            int colDiff = col - shipManager.CurrentShipPosition.X;
+            int rowDiff = row - shipManager.CurrentShipPosition.Y;
+            int offset;
+            int shipPosX;
+            int shipPosY;
+
+            for (int i = 0; i < shipManager.ShipeSizes[(int)shipManager.CurrentShipIndex]; i++)
+            {
+                if (colDiff != 0)
+                {
+                    offset = colDiff > 0 ? i : -i;
+                    shipPosX = shipManager.CurrentShipPosition.X + offset;
+                    shipPosY = shipManager.CurrentShipPosition.Y;
+                }
+                else
+                {
+                    offset = rowDiff > 0 ? i : -i;
+                    shipPosX = shipManager.CurrentShipPosition.X;
+                    shipPosY = shipManager.CurrentShipPosition.Y + offset;
+                }
+
+                shipManager.ShipPositions[(int)shipManager.CurrentShipIndex, i] = new Point(shipPosX, shipPosY);
+                DGV_AllyFleet[shipPosX, shipPosY].Style.BackColor = Color.Black;
+            }
+        }
+
+        private bool shipIsInTheWay(int col, int row)
+        {
+            int colDiff = col - shipManager.CurrentShipPosition.X;
+            int rowDiff = row - shipManager.CurrentShipPosition.Y;
+
+            for (int i = 0; i < shipManager.ShipeSizes[(int)shipManager.CurrentShipIndex]; i++)
+            {
+                if (colDiff != 0)
+                {
+                    int offset = colDiff > 0 ? i : -i;
+                    if (DGV_AllyFleet[shipManager.CurrentShipPosition.X + offset, shipManager.CurrentShipPosition.Y].Style.BackColor == Color.Black)
+                        return true;
+                }
+                else
+                {
+                    int offset = rowDiff > 0 ? i : -i;
+                    if (DGV_AllyFleet[shipManager.CurrentShipPosition.X, shipManager.CurrentShipPosition.Y + offset].Style.BackColor == Color.Black)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void resetShipPlacement()
+        {
+            for (int col = 0; col < DGV_AllyFleet.Columns.Count; col++)
+            {
+                for (int row = 0; row < DGV_AllyFleet.Rows.Count; row++)
+                {
+                    if (DGV_AllyFleet[col, row].Style.BackColor == Color.Green || DGV_AllyFleet[col, row].Style.BackColor == Color.Blue)
+                    {
+                        DGV_AllyFleet[col, row].Style.BackColor = Color.White;
+                    }
+                }
+            }
+        }
+
+        private void UpdateStatusLabel_ShipPlacement()
+        {
+            LBL_Status.Text = "Placez le " + shipManager.ShipNames[(int)shipManager.CurrentShipIndex];
+        }
+
+
+    }
+}
